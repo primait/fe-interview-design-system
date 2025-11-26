76d8956b3b0d04a3e8563ff994f983d3487513a7
Emanuele Macri <emanuele.macri@prima.it>
2025-11-26 09:15:38 +0100

    style: migrate to biome

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
+
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    include: ["src/**/*.test.{js,ts,tsx}"],
-    globals: true, //https://vitest.dev/guide/migration.html#globals-as-a-default
-    environment: "jsdom",
-    setupFiles: "./src/setupTests.ts",
-  },
+	plugins: [react()],
+	test: {
+		include: ["src/**/*.test.{js,ts,tsx}"],
+		globals: true, //https://vitest.dev/guide/migration.html#globals-as-a-default
+		environment: "jsdom",
+		setupFiles: "./src/setupTests.ts",
+	},
 });
