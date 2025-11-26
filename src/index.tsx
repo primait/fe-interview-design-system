9aebb44681098e7f40dfb4206548aa1b870b1b11
Emanuele Macri <emanuele.macri@prima.it>
2025-11-26 09:18:09 +0100

    style: fix

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const root = ReactDOM.createRoot(
-	document.getElementById("root") as HTMLElement,
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(<React.StrictMode>👋</React.StrictMode>);
--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,11 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	test: {
-		include: ["src/**/*.test.{js,ts,tsx}"],
-		globals: true, //https://vitest.dev/guide/migration.html#globals-as-a-default
-		environment: "jsdom",
-		setupFiles: "./src/setupTests.ts",
-	},
+  plugins: [react()],
+  test: {
+    include: ["src/**/*.test.{js,ts,tsx}"],
+    globals: true, //https://vitest.dev/guide/migration.html#globals-as-a-default
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+  },
 });
